4f30082082873143edff5f2240645c0f2f2a220e
Viktor Safar <viktor.safar@geodata.no>
2024-07-01 10:22:52 +0200

    update config base

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps : {
-    include : ['@bryntum/scheduler', '@bryntum/scheduler-react']
-}
-})
+  optimizeDeps: {
+    include: ['@bryntum/scheduler', '@bryntum/scheduler-react'],
+  },
+  base: '/gantt-entrepreneur/',
+});
